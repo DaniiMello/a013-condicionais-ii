789e3f07280a766bedd0317049dac273c0fea1cb
DaniiMello <daniellemello.p@gmail.com>
2022-09-08 14:55:17 -0300

    exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,13 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite seu número aqui"))
+//aninhados
+// if (num%2===0){
+//     if (num%3===0){
 
+//     }
+// }
+
+
+//uma operação só
+if (num%2===0 || num%3===0){
+    console.log(num," é divisível por 2 ou 3")
+}
